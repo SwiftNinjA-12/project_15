1c2d7ba45d9ba6b0230c780119a925d4053786f5
Tom Verveckken <tverveckken@gmail.com>
2022-10-31 14:46:50 +0100

    end of project end 473

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
+app.use('/images', express.static('images')); // this makes the folder visible to the browser
+// the '/images' before the middleware(filter) makes it so that only requests that have '/images' will 
+// use this middle ware and remove the /images from the incoming request path
 
 app.use(userRoutes);
 
--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,41 @@
-const express = require('express');
+const { renderFile } = require("ejs");
+const express = require("express");
+const multer = require("multer");
 
-const router = express.Router();
+const db = require("../data/database");
 
-router.get('/', function(req, res) {
-  res.render('profiles');
+const storageConfig = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "images/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
 });
 
-router.get('/new-user', function(req, res) {
-  res.render('new-user');
+const upload = multer({ storage: storageConfig });
+const router = express.Router();
+
+router.get("/", async function (req, res) {
+  const users = await db.getDb().collection('users').find().toArray();
+  res.render( "profiles", { users: users });
+});
+
+router.get("/new-user", function (req, res) {
+  res.render("new-user");
+});
+
+router.post("/profiles", upload.single("image"), async function (req, res) {
+  const uploadedImageFile = req.file;
+  const userData = req.body;
+
+  await db.getDb().collection("users").insertOne({
+    name: userData.username,
+    imagePath: uploadedImageFile.path,
+  });
+// console.log(uploadedImageFile)
+// console.log(userData)
+  res.redirect("/");
 });
 
 module.exports = router;
